5cd243d7576e8b91c1199c38e70e15b0d4ae0b29
Pranav Kumar <kumarpranav1987@gmail.com>
2020-02-20 12:14:02 +0530

    Update JenkinsApplication.java

--- src/main/java/com/livetechstudy/JenkinsApplication.java
+++ src/main/java/com/livetechstudy/JenkinsApplication.java
@@ -3,6 +3,7 @@ package com.livetechstudy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class JenkinsApplication {
 
